83f3bb42034bd3a418cbab31e1d4b76adbbec316
Christian <Christian@CR002-MBPro.local>
2014-11-10 09:51:56 +0100

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Christian on 10.11.14.
+//  Copyright (c) 2014 Christian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Christian on 10.11.14.
+//  Copyright (c) 2014 Christian. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
